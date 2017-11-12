1c6d2a498aada59c1aaabd45a13456cd6e51686b
Sergio Alexander <xergioalex@gmail.com>
2017-11-11 22:48:25 -0500

    Schema updated

--- schema.js
+++ schema.js
@@ -1,13 +1,40 @@
 const { makeExecutableSchema } = require('graphql-tools')
 
 const typeDefs = `
+  # Esto es un curso en el sistema
   type Curso {
     id: ID!
     titulo: String!
+    # Esta es la descripción del curso
+    descripcion: String!
+    profesor: Profesor
+    rating: Float @deprecated(reason: "No creemos más en los puntajes")
+    comentarios: [Comentario]
+  }
+
+  type Profesor {
+    id: ID!
+    nombre: String!
+    nacionalidad: String!
+    genero: Genero
+  }
+
+  enum Genero {
+    MASCULINO
+    FEMENINO 
+  }
+
+  type Comentario {
+    id: ID!
+    nombre: String!
+    comentario: String!
   }
 
   type Query {
     cursos: [Curso]
+    profesores: [Profesor]
+    curso(id: Int): Curso
+    profesor(id: Int): Profesor
   }
 `
 
