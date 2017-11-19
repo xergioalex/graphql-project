2ae7ff73f36d1c9c73535dd690c8c188206293b7
Sergio Alexander <xergioalex@gmail.com>
2017-11-19 16:33:54 -0500

    Mutations added for add, edit, delete

---
+++ resolvers.js
@@ -0,0 +1,26 @@
+const Curso = require('./models/Curso')
+const Profesor = require('./models/Profesor')
+
+const resolvers = {
+  Query: {
+    cursos: () => Curso.query().eager('[profesor, comentarios]'),
+    profesores: () => Profesor.query().eager('cursos'),
+    curso: (rootValue, args) => Curso.query().eager('[profesor, comentarios]').findById(args.id),
+    profesor: (rootValue, args) => Profesor.query().eager('cursos').findById(args.id)
+  },
+  Mutation: {
+  	profesorAdd: (_, args) => {
+  		return Profesor.query().insert(args.profesor)
+  	},
+  	profesorEdit: (_, args) => {
+    	return Profesor.query().patchAndFetchById(args.profesorId, args.profesor)
+    },
+    profesorDelete: (_, args) => {
+      	return Profesor.query().findById(args.profesorId).then((profesor) => {
+        	return Profesor.query().deleteById(args.profesorId).then(() => profesor)
+      	})
+    }
+  }
+}
+
+module.exports = resolvers
---
+++ schema/index.js
@@ -0,0 +1,26 @@
+const { makeExecutableSchema } = require('graphql-tools')
+const resolvers = require('../resolvers')
+const Profesor = require('./Profesor')
+const Curso = require('./Curso')
+
+const rootQuery = `
+  type Query {
+    cursos: [Curso]
+    profesores: [Profesor]
+    curso(id: Int): Curso
+    profesor(id: Int): Profesor
+  }
+
+  type Mutation {
+  	profesorAdd(profesor: NuevoProfesor): Profesor
+  	profesorEdit(profesorId: Int!, profesor: ProfesorEditable): Profesor
+  	profesorDelete(profesorId: Int!): Profesor
+  }
+`
+
+const schema = makeExecutableSchema({
+  typeDefs: [rootQuery, Profesor, Curso],
+  resolvers
+})
+
+module.exports = schema
