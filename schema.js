9f034aa316718dc780cc7da58e9b5f517660ba24
Sergio Alexander <xergioalex@gmail.com>
2017-11-19 14:10:09 -0500

    Resolvers added

--- schema.js
+++ schema.js
@@ -27,17 +27,86 @@ const typeDefs = `
   type Comentario {
     id: ID!
     nombre: String!
-    comentario: String!
+    cuerpo: String!
   }
 
   type Query {
     cursos: [Curso]
+    comentarios: [Comentario]
     profesores: [Profesor]
     curso(id: Int): Curso
     profesor(id: Int): Profesor
   }
 `
 
-const schema = makeExecutableSchema({ typeDefs })
+const resolvers = {
+  Query: {
+    cursos: () => {
+      return [{
+        id: 1,
+        titulo: 'Curso de GraphQL',
+        descripcion: 'Aprendiendo GraphQL',
+        profesor: {
+          nombre: 'Sergio'
+        }
+      },
+      {
+        id: 2,
+        titulo: 'Curso de Javascript',
+        descripcion: 'Aprendiendo Javascript'
+      }]
+    },
+    comentarios: () => {
+      return [
+        {
+          id: 1,
+          nombre: 'Comentario 1',
+          cuerpo: 'Cuerpo comentario 1'
+        },
+        {
+          id: 2,
+          nombre: 'Comentario 2',
+          cuerpo: 'Cuerpo comentario 2'
+        },
+        {
+          id: 3,
+          nombre: 'Comentario 3',
+          cuerpo: 'Cuerpo comentario 3'
+        },
+        {
+          id: 4,
+          nombre: 'Comentario 4',
+          cuerpo: 'Cuerpo comentario 4'
+        }
+      ]
+    }
+  },
+  Curso: {
+    profesor: () => {
+      return {
+        nombre: 'luis',
+        nacionalidad: 'colombia'
+      }
+    },
+    comentarios: () => {
+      return [{
+        id:1,
+        nombre: 'luisj135',
+        cuerpo: 'testing GraphQl'
+      },
+      {
+        id:2,
+        nombre: 'Platzi',
+        cuerpo: 'testing Platzi GraphQl'
+      }]
+    }
+  }
+}
+
+const schema = makeExecutableSchema({ 
+  typeDefs,
+  resolvers
+
+})
 
 module.exports = schema
